65ab1b6e9cd352cfed1ee66a44dced581340cbd4
Mateus Cesar <mateuscesarglima@gmail.com>
2023-02-22 22:51:46 -0300

    feat(BALLEBOT-3): Add theme type

---
+++ src/global/themes/theme.ts
@@ -0,0 +1,17 @@
+export default {
+  colors: {
+    primary: "#FFF2E7",
+    secondary: "#ECD6C4",
+    tertiary: "#2F2325",
+  },
+  fonts: {
+    page_title: {
+      font: "'Righteous', sans-serif",
+      weight: 400,
+    },
+    nav_link: {
+      font: "'Sarala', sans-serif",
+      weight: 400,
+    },
+  },
+};
---
+++ src/styles.ts
@@ -0,0 +1,5 @@
+import styled from "styled-components";
+
+export const Teste = styled.p`
+  font-family: ${({ theme }) => theme.fonts.page_title};
+`;
